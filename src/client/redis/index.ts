94ed4e058aaf45255f07cf63de0d17b8be7e7d91
anshul1822 <anshulanant19@gmail.com>
2024-03-28 16:22:29 +0530

    env

--- src/client/redis/index.ts
+++ src/client/redis/index.ts
@@ -1,3 +1,3 @@
 import Redis from "ioredis"
 
-export const redisClient = new Redis("redis://default:df0d1ec4fdd04dd8b233a48747366185@eu1-enabled-crane-38265.upstash.io:38265");
+export const redisClient = new Redis(process.env.REDIS_URL as string);
